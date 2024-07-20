c6e407abc04018e30b3fa818d1ec9a0dba6f1d2c
daniel <daniel@deb.sys_domain>
2024-07-20 12:18:02 -0500

    uniqueness

--- process_register.php
+++ process_register.php
@@ -1,54 +1,82 @@
-<html>
-	<head>
-		<title>Main Register Page</title>
-	</head>
-	<body>
+<?php
+ob_start();
+require 'config.php';
+require 'database.php';
 
-		<?php
+$host = "localhost"; // Host name
+$username = "bloguser"; // Mysql username
+$password = "password"; // Mysql password
+$db_name = "blog"; // Database name
+$tbl_name = "members"; // Table name
 
-		ob_start();
-		$host="localhost"; // Host name
-		$username="bloguser"; // Mysql username
-		$password="password"; // Mysql password
-		$db_name="blog"; // Database name
-		$tbl_name="members"; // Table name
+$mysqli = new mysqli($host, $username, $password, $db_name);
 
-		$mysqli = new mysqli($host, $username, $password, $db_name);
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
 
-		/* check connection */
-		if ($mysqli->connect_errno) {
-			printf("Connect failed: %s\n", $mysqli->connect_error);
-			exit();
-		}
-		$myusername = filter_input(INPUT_POST, 'myusername', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-		$myemail= filter_input(INPUT_POST, 'myemail', FILTER_VALIDATE_EMAIL);
-		$mypassword=$_POST['mypassword'];
+$myusername = filter_input(INPUT_POST, 'myusername', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$myemail = filter_input(INPUT_POST, 'myemail', FILTER_VALIDATE_EMAIL);
+$mypassword = $_POST['mypassword'];
 
-		// To protect MySQL injection (more detail about MySQL injection)
-		$cleanusername = $myusername;
-		$cleanemail = $myemail;
-		$cleanpassword = $mypassword;
+$errors = [];
 
-		// salting adds uniqueness to each entry.
-		$salt=uniqid() ;
-		$salted_password=$salt.$cleanpassword;
-		$encrypted_password = hash("sha512", $salted_password); 
+if (!$myusername) {
+    $errors['username'] = 'Username is required.';
+}
 
-		$sql="insert into $tbl_name (myusername,myemail,mypassword,salt) values ('$cleanusername','$cleanemail','$encrypted_password','$salt')";
-		if (!$mysqli->query($sql)) {
-			echo "INSERT failed: (" . $mysqli->errno . ") " . $mysqli->error;
-		}
-		else
-		{
-			// echo "Registered";
-			header ("Location: login.php");
-		}
-		printf("SQL statement is $sql");
-		ob_end_flush();
-		?>
+if (!$myemail) {
+    $errors['email'] = 'A valid email is required.';
+}
 
+// Check if username or email already exists
+if ($myusername) {
+    $stmt = $mysqli->prepare("SELECT COUNT(*) FROM $tbl_name WHERE myusername = ?");
+    $stmt->bind_param('s', $myusername);
+    $stmt->execute();
+    $stmt->bind_result($username_count);
+    $stmt->fetch();
+    $stmt->close();
 
+    if ($username_count > 0) {
+        $errors['username'] = 'Username is already taken.';
+    }
+}
 
+if ($myemail) {
+    $stmt = $mysqli->prepare("SELECT COUNT(*) FROM $tbl_name WHERE myemail = ?");
+    $stmt->bind_param('s', $myemail);
+    $stmt->execute();
+    $stmt->bind_result($email_count);
+    $stmt->fetch();
+    $stmt->close();
 
-	</body>
-</html>
+    if ($email_count > 0) {
+        $errors['email'] = 'Email is already registered.';
+    }
+}
+
+if (!empty($errors)) {
+    $query_params = http_build_query(['errors' => json_encode($errors), 'myusername' => $myusername, 'myemail' => $myemail]);
+    header("Location: register.php?" . $query_params);
+    exit();
+}
+
+// salting adds uniqueness to each entry.
+$salt = uniqid();
+$salted_password = $salt . $mypassword;
+$encrypted_password = hash("sha512", $salted_password);
+
+$stmt = $mysqli->prepare("INSERT INTO $tbl_name (myusername, myemail, mypassword, salt) VALUES (?, ?, ?, ?)");
+$stmt->bind_param('ssss', $myusername, $myemail, $encrypted_password, $salt);
+
+if (!$stmt->execute()) {
+    echo "INSERT failed: (" . $stmt->errno . ") " . $stmt->error;
+} else {
+    header("Location: login.php");
+}
+$stmt->close();
+ob_end_flush();
+?>
--- register.php
+++ register.php
@@ -1,54 +1,57 @@
 <?php
-  require 'config.php';
-  require 'database.php';
-  $g_title = BLOG_NAME . ' - Index';
-  $g_page = 'index';
-  require 'header.php';
-  require 'menu.php';
-  
-  $posts = find_all_blogs(BLOG_INDEX_NUM_POSTS);
+require 'config.php';
+require 'database.php';
+$g_title = BLOG_NAME . ' - Register';
+$g_page = 'register';
+require 'header.php';
+require 'menu.php';
+
+$myusername = isset($_POST['myusername']) ? htmlspecialchars($_POST['myusername']) : '';
+$myemail = isset($_POST['myemail']) ? htmlspecialchars($_POST['myemail']) : '';
+$errors = isset($_GET['errors']) ? json_decode(urldecode($_GET['errors']), true) : [];
+
 ?>
 
 <div id="all_blogs">
-
-		<table width="300" border="0" cellpadding="0" cellspacing="1">
-			<tr>
-				<form name="form1" method="post" action="process_register.php">
-					<td>
-						<table width="100%" border="0" cellpadding="3" cellspacing="1">
-							<tr>
-								<td colspan="3"><strong>Member Register </strong></td>
-							</tr>
-							<tr>
-								<td width="78">Username</td>
-								<td width="6">:</td>
-								<td width="294"><input name="myusername" type="text" id="myusername"></td>
-							</tr>
-							<tr>
-								<td width="78">Email</td>
-								<td width="6">:</td>
-								<td width="294"><input name="myemail" type="email" id="myemail"></td>
-							</tr>
-							<tr>
-								<td>Password</td>
-								<td>:</td>
-								<td><input name="mypassword" type="password" id="mypassword"></td>
-							</tr>
-							<!-- <tr>
-								<td>Verify Password</td>
-								<td>:</td>
-								<td><input name="mypassword-verify" type="password" id="mypassword-verify"></td>
-							</tr> -->
-							<tr>
-								<td>&nbsp;</td>
-								<td>&nbsp;</td>
-								<td><input type="submit" name="Submit" value="Register"></td>
-							</tr>
-						</table>
-					</td>
-				</form>
-			</tr>
-		</table>
+    <table width="300" border="0" cellpadding="0" cellspacing="1">
+        <tr>
+            <form name="form1" method="post" action="process_register.php">
+                <td>
+                    <table width="100%" border="0" cellpadding="3" cellspacing="1">
+                        <tr>
+                            <td colspan="3"><strong>Member Register </strong></td>
+                        </tr>
+                        <tr>
+                            <td width="78">Username</td>
+                            <td width="6">:</td>
+                            <td width="294"><input name="myusername" type="text" id="myusername" value="<?php echo $myusername; ?>"></td>
+                        </tr>
+                        <tr>
+                            <td colspan="3"><?php if(isset($errors['username'])) echo $errors['username']; ?></td>
+                        </tr>
+                        <tr>
+                            <td width="78">Email</td>
+                            <td width="6">:</td>
+                            <td width="294"><input name="myemail" type="email" id="myemail" value="<?php echo $myemail; ?>"></td>
+                        </tr>
+                        <tr>
+                            <td colspan="3"><?php if(isset($errors['email'])) echo $errors['email']; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Password</td>
+                            <td>:</td>
+                            <td><input name="mypassword" type="password" id="mypassword"></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td><input type="submit" name="Submit" value="Register"></td>
+                        </tr>
+                    </table>
+                </td>
+            </form>
+        </tr>
+    </table>
 </div>
 
-<?php require 'footer.php';?>
+<?php require 'footer.php'; ?>
